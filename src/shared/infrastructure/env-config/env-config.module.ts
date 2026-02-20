0b74143d257e755e02dafb06c27de157ae389d69
Felipe Moreira Rios <feliperioscross.br@gmail.com>
2026-02-20 15:13:28 -0300

    feat(users): add shared domain structure and env config tests

--- src/shared/infrastructure/env-config/env-config.module.ts
+++ src/shared/infrastructure/env-config/env-config.module.ts
@@ -1,24 +1,18 @@
 import { DynamicModule, Module } from '@nestjs/common'
-import { join } from 'node:path'
-import { ConfigModule, ConfigModuleOptions } from '@nestjs/config'
 import { EnvConfigService } from './env-config.service'
+import { ConfigModule, ConfigModuleOptions } from '@nestjs/config'
+import { join } from 'node:path'
 
 @Module({
   providers: [EnvConfigService],
 })
-export class EnvConfigModule {
+export class EnvConfigModule extends ConfigModule {
   static forRoot(options: ConfigModuleOptions = {}): DynamicModule {
-    return {
-      module: EnvConfigModule,
-      imports: [
-        ConfigModule.forRoot({
-          ...options,
-          isGlobal: true,
-          envFilePath: [
-            join(__dirname, `../../../../.env.${process.env.NODE_ENV}`),
-          ],
-        }),
+    return super.forRoot({
+      ...options,
+      envFilePath: [
+        join(__dirname, `../../../../.env.${process.env.NODE_ENV}`),
       ],
-    }
+    })
   }
 }
